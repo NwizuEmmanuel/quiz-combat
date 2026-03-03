e556cc30e1f3ae8439c3bd09b964aacf41ea985b
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2026-03-03 17:44:34 +0800

    refactor: update quiz editor to improve answer handling and UI elements

--- scripts/quiz_editor.gd
+++ scripts/quiz_editor.gd
@@ -5,7 +5,8 @@ extends Control
 @onready var option_2_input = $MarginContainer/HSplitContainer/VBoxContainer/Option2
 @onready var option_3_input = $MarginContainer/HSplitContainer/VBoxContainer/Option3
 @onready var option_4_input = $MarginContainer/HSplitContainer/VBoxContainer/Option4
-@onready var answer_input = $MarginContainer/HSplitContainer/VBoxContainer/Answer
+@onready var identification_answer_input = $MarginContainer/HSplitContainer/VBoxContainer/IdentificationAnswer
+@onready var multiple_choice_answer_input = $MarginContainer/HSplitContainer/VBoxContainer/MultipleChoiceAnswer
 @onready var multiple_choice_checkbutton = $MarginContainer/HSplitContainer/VBoxContainer/MultipleChoice  
 @onready var questions_item_list = $MarginContainer/HSplitContainer/VBoxContainer2/QuestionsList
 @onready var add_update_button = $MarginContainer/HSplitContainer/VBoxContainer/HBoxContainer/AddUpdate
@@ -18,9 +19,11 @@ extends Control
 @onready var option_3_error_label = $MarginContainer/HSplitContainer/VBoxContainer/Option3Error
 @onready var option_4_error_label = $MarginContainer/HSplitContainer/VBoxContainer/Option4Error
 @onready var answer_error_label = $MarginContainer/HSplitContainer/VBoxContainer/AnswerError
-@onready var answer_label = $MarginContainer/HSplitContainer/VBoxContainer/AnswerLabel
+@onready var multiple_choice_answer_label = $MarginContainer/HSplitContainer/VBoxContainer/MultipleChoiceAnswerLabel
+
 
 var questions = []
+var updating_question = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hide_multiple_choice()
@@ -29,13 +32,6 @@ func _ready() -> void:
 	clear_inputs_button.connect("pressed", clear_inputs)
 	delete_button.connect("pressed", delete_question)
 	clear_selections_button.connect("pressed", clear_questions_item_list_selections)
-	answer_input.connect("text_changed", multiple_choice_answer_text_change)
-
-
-func multiple_choice_answer_text_change(new_text):
-	if multiple_choice_checkbutton.button_pressed:
-		if new_text.length() > 1 and !["1","2","3","4"].has(new_text):
-			answer_input.text = ""
 
 
 # deselect all selections
@@ -56,7 +52,8 @@ func clear_inputs():
 	option_2_input.clear()
 	option_3_input.clear()
 	option_4_input.clear()
-	answer_input.clear()
+	identification_answer_input.clear()
+	multiple_choice_answer_input.value = 1.0
 
 # add new question or update existing question
 func add_update_question():
@@ -76,23 +73,27 @@ func refresh_questions():
 func add_identification():
 	var duration = 5.0
 	var question = question_input.text.strip_edges()
-	var answer = answer_input.text.strip_edges()
+	var answer = identification_answer_input.text.strip_edges()
 	# validate question and answer inputs
 	if question != "" and answer != "":
 		var question_item = {
-			"type": "identification",
-			"question": question.to_lower(),
-			"answer": answer.to_lower()
+				"type": "identification",
+				"question": question.to_lower(),
+				"answer": answer.to_lower()
 		}
-		questions.append(question_item)
-		
+		if updating_question:
+			var selected_index = questions_item_list.get_selected_items()[0]
+			questions[selected_index] = question_item
+			updating_question = false
+		else:
+			questions.append(question_item)
 		clear_inputs()
 		refresh_questions()
 	else:
-		if question_input.text == "":
+		if question == "":
 			question_error_label.show()
 			question_error_label.text = "question can't be empty"
-		if answer_input.text == "":
+		if answer == "":
 			answer_error_label.show()
 			answer_error_label.text = "answer can't be empty"
 		await get_tree().create_timer(duration).timeout
@@ -106,37 +107,41 @@ func add_multiple_choice():
 	var option3 = option_3_input.text.strip_edges().to_lower()
 	var option4 = option_4_input.text.strip_edges().to_lower()
 	var question = question_input.text.strip_edges().to_lower()
-	var answer = answer_input.text.strip_edges().to_lower()
-	if option1 != "" and option2 != "" and option3 != "" and option4 != "" and question != "" and answer != "":
+	var answer = multiple_choice_answer_input.value
+	if option1 != "" and option2 != "" and option3 != "" and option4 != "" and question != "" and (answer > 0 or answer < 5):
 		var question_item = {
 			"type": "multiple_choice",
 			"question": question,
 			"options": [option1, option2, option3, option4],
-			"answer": answer
+			"answer": str(answer)
 		}
-		questions.append(question_item)
-		
+		if updating_question:
+			var selected_index = questions_item_list.get_selected_items()[0]
+			questions[selected_index] = question_item
+			updating_question = false
+		else:
+			questions.append(question_item)
 		clear_inputs()
 		refresh_questions()
 	else:
-		if question_input.text == "":
+		if question == "":
 			question_error_label.show()
 			question_error_label.text = "question can't be empty"
-		if option_1_input.text == "":
+		if option1 == "":
 			option_1_error_label.show()
 			option_1_error_label.text = "option 1 can't be empty"
-		if option_2_input.text == "":
+		if option2 == "":
 			option_2_error_label.show()
 			option_2_error_label.text = "option 2 can't be empty"
-		if option_3_input.text == "":
+		if option3 == "":
 			option_3_error_label.show()
 			option_3_error_label.text = "option 3 can't be empty"
-		if option_4_input.text == "":
+		if option4 == "":
 			option_4_error_label.show()
 			option_4_error_label.text = "option 4 can't be empty"
-		if answer_input.text == "":
+		if answer < 1 or answer > 4:
 			answer_error_label.show()
-			answer_error_label.text = "answer can't be empty"
+			answer_error_label.text = "answer be between 1-4"
 		await get_tree().create_timer(duration).timeout
 		question_error_label.hide()
 		option_1_error_label.hide()
@@ -147,27 +152,39 @@ func add_multiple_choice():
 
 # hide text inputs related to multiple choice
 func hide_multiple_choice():
-	answer_input.show()
+	multiple_choice_answer_label.hide()
+	multiple_choice_answer_input.hide()
 	option_1_input.hide()
 	option_2_input.hide()
 	option_3_input.hide()
 	option_4_input.hide()
 
+# show text inputs related to multiple choice
+func show_multiple_choice():
+	multiple_choice_answer_input.show()
+	multiple_choice_answer_label.show()
+	option_1_input.show()
+	option_2_input.show()
+	option_3_input.show()
+	option_4_input.show()
+
+
+# hide text inputs related to identification
+func hide_identification():
+	identification_answer_input.hide()
+
+# show text inputs related to identification
+func show_identification():
+	identification_answer_input.show()
+
 # toggle text inputs for multiple choice or identification
 func toggle_multiple_choice(toggled_on: bool):
 	if toggled_on:
-		answer_input.show()
-		answer_label.show()
-		option_1_input.show()
-		option_2_input.show()
-		option_3_input.show()
-		option_4_input.show()
+		show_multiple_choice()
+		hide_identification()
 	else:
-		option_1_input.hide()
-		option_2_input.hide()
-		option_3_input.hide()
-		option_4_input.hide()
-		answer_label.hide()
+		show_identification()
+		hide_multiple_choice()
 	question_error_label.hide()
 	option_1_error_label.hide()
 	option_2_error_label.hide()
@@ -178,3 +195,19 @@ func toggle_multiple_choice(toggled_on: bool):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_questions_list_item_activated(index: int) -> void:
+	updating_question = true
+	var question_type = questions[index]["type"]
+	question_input.text = questions[index]["question"]
+	if question_type == "multiple_choice":
+		multiple_choice_checkbutton.button_pressed = true
+		option_1_input.text = questions[index]["options"][0]
+		option_2_input.text = questions[index]["options"][1]
+		option_3_input.text = questions[index]["options"][2]
+		option_4_input.text = questions[index]["options"][3]
+		multiple_choice_answer_input.value = int(questions[index]["answer"])
+	else:
+		multiple_choice_checkbutton.button_pressed = false
+		identification_answer_input.text = questions[index]["answer"]
