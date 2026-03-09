502c44d5d6ba9da41fc59cc9c3ee8708ccd6315b
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2026-03-10 00:04:25 +0800

    Add UI components for quiz editing and question selection
    - Introduced new font file for UI styling.
    - Created a theme resource for consistent UI appearance.
    - Added IdentificationEditor scene for editing identification questions.
    - Implemented MultipleChoiceEditor scene with layout for multiple choice questions.
    - Developed QuestionType scene to allow users to select question types.

--- scripts/hub.gd
+++ scripts/hub.gd
@@ -1,30 +1,10 @@
-extends CanvasLayer
+extends Control
 
-@onready var start_game_btn = $Panel/VBox/StartGame
-@onready var exit_btn = $Panel/VBox/Exit
 
-var busy = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	start_game_btn.connect("pressed",_on_start_game_btn_pressed)
-	start_game_btn.connect("mouse_entered", play_hover_sound) 
-	exit_btn.connect("pressed", _on_exit_btn_pressed)
-	exit_btn.connect("mouse_entered", play_hover_sound)
+	pass # Replace with function body.
 
-func _on_start_game_btn_pressed():
-	if busy:
-		return
-	busy = true
-	SoundsEffect.play_sound(preload("res://sounds/button_press.mp3"))  
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
-	busy = true
 
-func _on_exit_btn_pressed():
-	if busy:
-		return
-	busy = true
-	SoundsEffect.play_sound(preload("res://sounds/button_press.mp3"))
-	get_tree().quit()
-
-func play_hover_sound():
-	SoundsEffect.play_sound(preload("res://sounds/button_hover.mp3"))
+func _on_create_quiz_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/question_type.tscn")
